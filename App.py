799bf6d9a88d9365949c3f25d58016e28ffe6a40
paolosmart <35848572+paolosmart@users.noreply.github.com>
2023-05-18 12:54:52 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,107 @@
+import os
+import tkinter as tk
+from tkinter import messagebox, simpledialog, filedialog
+from lista_titoli import ListaTitoli  # Assicurarsi che lista_titoli.py sia nello stesso directory
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Gestione Lista Titoli')
+
+        # Configura la finestra
+        self.root.geometry('800x600+400+300')  # Finestra di dimensione 800x600, centrata
+        self.root.configure(bg='black')  # Colore di sfondo nero
+
+        # Ottieni un elenco dei file nella directory corrente
+        self.files = os.listdir('.')
+    
+        # Filtra l'elenco per includere solo i file che terminano con '.txt'
+        self.list_files = [file for file in self.files if file.endswith('.txt')]
+
+        # Se non ci sono file di lista, inizia con una lista vuota
+        if not self.list_files:
+            self.lista = ListaTitoli()
+        else:
+            # Altrimenti, inizia con il primo file di lista
+            self.lista = ListaTitoli(self.list_files[0])
+        
+        # Crea il bottone per creare una nuova lista
+        self.nuova_lista_button = tk.Button(root, text="Crea nuova lista", command=self.crea_nuova_lista)
+        self.nuova_lista_button.pack()
+
+        # Crea il menu a tendina
+        self.selected_file = tk.StringVar(self.root)
+        self.selected_file.set(self.lista.file_name)  # imposta l'opzione predefinita
+        self.dropdown = tk.OptionMenu(self.root, self.selected_file, *self.list_files, command=self.seleziona_lista)
+        self.dropdown.configure(bg='black', fg='white')  # Configura il colore di sfondo e del testo
+        self.dropdown.pack()
+
+        # Crea la Listbox per visualizzare i titoli
+        self.listbox = tk.Listbox(self.root, bg='black', fg='white')
+        self.listbox.pack(fill=tk.BOTH, expand=1)
+
+        # Creazione dei bottoni
+        self.aggiungi_bottone = tk.Button(root, text="Aggiungi Titolo", command=self.aggiungi_titolo, bg='black', fg='white')
+        self.salva_bottone = tk.Button(root, text="Salva Lista", command=self.salva_lista, bg='black', fg='white')
+        self.rimuovi_bottone = tk.Button(root, text="Rimuovi Titolo", command=self.rimuovi_titolo, bg='black', fg='white')
+
+        # Posizionamento dei bottoni
+        self.aggiungi_bottone.pack(fill=tk.BOTH)
+        self.salva_bottone.pack(fill=tk.BOTH)
+        self.rimuovi_bottone.pack(fill=tk.BOTH)
+
+    def seleziona_lista(self, file_name):
+        self.lista = ListaTitoli(file_name)
+        self.aggiorna_elenco()
+        messagebox.showinfo("Successo", f"Lista '{file_name}' selezionata con successo!")
+    
+    def crea_nuova_lista(self):
+        self.lista = ListaTitoli()  # create a new empty list
+        self.update_lista_display()  # update the list display
+        self.update_option_menu()  # update the dropdown menu
+
+    def aggiungi_titolo(self):
+        titolo = simpledialog.askstring("Aggiungi Titolo", "Inserisci il titolo da aggiungere:")
+        if titolo:
+            self.lista.aggiungi_titolo(titolo)
+            self.aggiorna_elenco()
+            messagebox.showinfo("Successo", f"Titolo '{titolo}' aggiunto con successo!")
+
+    def salva_lista(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text file", "*.txt"),("All Files", "*.*") ))
+        if file_path:
+            self.lista.salva_lista(file_path)
+            self.aggiorna_menu(file_path)
+            messagebox.showinfo("Successo", f"Lista salvata come '{file_path}'!")
+
+    def rimuovi_titolo(self):
+        titolo = simpledialog.askstring("Rimuovi Titolo", "Inserisci il titolo da rimuovere:")
+        if titolo:
+            self.lista.rimuovi_titolo(titolo)
+            self.aggiorna_elenco()
+            messagebox.showinfo("Successo", f"Titolo '{titolo}' rimosso con successo!")
+
+    def aggiorna_elenco(self):
+        self.listbox.delete(0, tk.END)  # Rimuove tutti gli elementi esistenti
+        for titolo in self.lista.titoli:
+            self.listbox.insert(tk.END, titolo)  # Aggiunge ogni titolo alla Listbox
+
+    def aggiorna_menu(self, file_path):
+        # Aggiungi il nuovo file all'elenco dei file
+        self.list_files.append(os.path.basename(file_path))
+        # Ricrea il menu a tendina con l'elenco dei file aggiornato
+        self.dropdown['menu'].delete(0, 'end')
+        for file_name in self.list_files:
+            self.dropdown['menu'].add_command(label=file_name, command=tk._setit(self.selected_file, file_name, self.seleziona_lista))
+    
+    def update_option_menu(self):
+        menu = self.om["menu"]
+        menu.delete(0, "end")
+        for string in self.options:
+            menu.add_command(label=string, 
+                            command=lambda value=string: self.om_variable.set(value))
+
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
---
+++ lista_titoli.py
@@ -0,0 +1,37 @@
+#Lo scopo è recuperare tramite Api Directa i dati finanziari in tempo reale.
+#L'utente inizialmente dovrà scegliere quale lista titoli usare.
+#La lista titoli dovrà essere modificabile e salvata in un file. La lista titoli scelta dall'utente sarà poi una variabile usata
+#e passata al comando di sottoscrizione. In questo link puoi trovare  le istruzioni https://app1.directatrading.com/trading-api-directa/index.html.
+#Ora ponendo attenzione al formato ed alla posizione dei dati ricevuti dobbiamo salvarli in modo efficente in un file.
+#Solo successivamente ci occuperemo di rappresentare i dati in una tabella in tempo reale
+#ed grafico in tempo reale dando la possibilità all'utente di scegliere quali dati per la tabella e quali per il grafico. Quindi procediamo per passi.
+#Il software deve essere modulare in modo da poter lavorare sui singoli moduli separatamente.Tenendo in considerazione questi concetti procediamo.
+
+#Crea un codice python che si occupi della gestione lista titoli.
+import os.path
+class ListaTitoli:
+    def __init__(self, file_name='lista_titoli.txt'):
+        self.file_name = file_name
+        self.titoli = []
+        # Carica i titoli dal file, se esiste
+        if os.path.isfile(self.file_name):
+            with open(self.file_name, 'r') as f:
+                self.titoli = [line.strip() for line in f]
+
+    def aggiungi_titolo(self, titolo):
+        if titolo not in self.titoli:
+            self.titoli.append(titolo)
+
+    def rimuovi_titolo(self, titolo):
+        if titolo in self.titoli:
+            self.titoli.remove(titolo)
+
+    def salva_lista(self, file_name=None):
+        # Se non viene fornito un nome di file, usa il nome di file esistente
+        if file_name is None:
+            file_name = self.file_name
+        with open(file_name, 'w') as f:
+            for titolo in self.titoli:
+                f.write(titolo + '\n')
+
+
